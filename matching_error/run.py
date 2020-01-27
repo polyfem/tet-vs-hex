600f5659860e70b4c8b554244e8089550930272a
Teseo Schneider <teseo@bluewin.ch>
2020-01-27 17:32:38 -0500

    added q2 reduced

--- matching_error/run.py
+++ matching_error/run.py
@@ -9,7 +9,7 @@ if __name__ == '__main__':
     out_folder = "err"
     refs = [0, 1]
 
-    p2s = [[], ["square_beam.off_20.mesh"]
+    p2s = [[], [] #["square_beam.off_20.mesh"]
         # ["square_beam.off_1.mesh", "square_beam.off_10.mesh", "square_beam.off_20.mesh", "square_beam.off_5.mesh", "square_beam.off_50.mesh", "square_beam.off_6.mesh", "square_beam.off_7.mesh", "square_beam.off_8.mesh", "square_beam.off_9.mesh"],
         # ["square_beam.off_1.mesh", "square_beam.off_5.mesh", "square_beam.off_6.mesh", "square_beam.off_7.mesh", "square_beam.off_8.mesh", "square_beam.off_9.mesh", "square_beam.off_10.mesh", "square_beam.off_20.mesh"]
     ]
@@ -17,7 +17,13 @@ if __name__ == '__main__':
         # ["square_beam.off_1.HYBRID", "square_beam.off_10.HYBRID", "square_beam.off_20.HYBRID", "square_beam.off_5.HYBRID", "square_beam.off_50.HYBRID", "square_beam.off_6.HYBRID", "square_beam.off_7.HYBRID", "square_beam.off_8.HYBRID", "square_beam.off_9.HYBRID"],
         # ["square_beam.off_1.HYBRID", "square_beam.off_5.HYBRID", "square_beam.off_6.HYBRID", "square_beam.off_7.HYBRID", "square_beam.off_8.HYBRID", "square_beam.off_9.HYBRID", "square_beam.off_10.HYBRID"]
     ]
-    splines = [[], ["spline_square_beam.off_20.HYBRID"]
+
+    q2s = [
+        ["spline_square_beam.off_1.HYBRID", "spline_square_beam.off_10.HYBRID", "spline_square_beam.off_20.HYBRID", "spline_square_beam.off_5.HYBRID", "spline_square_beam.off_50.HYBRID", "spline_square_beam.off_6.HYBRID", "spline_square_beam.off_7.HYBRID", "spline_square_beam.off_8.HYBRID", "spline_square_beam.off_9.HYBRID"],
+        ["spline_square_beam.off_1.HYBRID", "spline_square_beam.off_5.HYBRID", "spline_square_beam.off_6.HYBRID", "spline_square_beam.off_7.HYBRID", "spline_square_beam.off_8.HYBRID", "spline_square_beam.off_9.HYBRID", "spline_square_beam.off_10.HYBRID", "spline_square_beam.off_20.HYBRID"]
+    ]
+
+    splines = [[], [] #["spline_square_beam.off_20.HYBRID"]
         # ["spline_square_beam.off_1.HYBRID", "spline_square_beam.off_10.HYBRID", "spline_square_beam.off_20.HYBRID", "spline_square_beam.off_5.HYBRID", "spline_square_beam.off_50.HYBRID", "spline_square_beam.off_6.HYBRID", "spline_square_beam.off_7.HYBRID", "spline_square_beam.off_8.HYBRID", "spline_square_beam.off_9.HYBRID"],
         # ["spline_square_beam.off_1.HYBRID", "spline_square_beam.off_5.HYBRID", "spline_square_beam.off_6.HYBRID", "spline_square_beam.off_7.HYBRID", "spline_square_beam.off_8.HYBRID", "spline_square_beam.off_9.HYBRID", "spline_square_beam.off_10.HYBRID", "spline_square_beam.off_20.HYBRID"]
     ]
@@ -37,9 +43,12 @@ if __name__ == '__main__':
         for n_refs in refs:
             p2 = p2s[n_refs]
             q1 = q1s[n_refs]
+            q2 = q2s[n_refs]
             spline = splines[n_refs]
 
             json_data["n_refs"] = n_refs
+            json_data["quadrature_order"] = 4
+
             print("------------------")
             print("P2")
             print("------------------")
@@ -87,6 +96,33 @@ if __name__ == '__main__':
 
                     subprocess.run(args)
 
+            #######################################################################
+            print("------------------")
+            print("Q2")
+            print("------------------")
+            for mesh_name in q2:
+                mesh = os.path.join("matching_error", mesh_name)
+                basename = os.path.splitext(os.path.basename(mesh))[0].replace("spline_square_beam.off_", "")
+                mesh = os.path.join(current_folder, mesh)
+
+                json_data["mesh"] = mesh
+                json_data["discr_order"] = 2
+                json_data["quadrature_order"] = 2
+                json_data["use_spline"] = False
+                json_data["output"] = os.path.join(
+                    current_folder, out_folder, run_f, "Q2_" + basename + "_" + str(n_refs) + ".json")
+
+                with tempfile.NamedTemporaryFile(suffix=".json") as tmp_json:
+                    with open(tmp_json.name, 'w') as f:
+                        f.write(json.dumps(json_data, indent=4))
+
+                    args = [polyfem_exe,
+                            '--json', tmp_json.name,
+                            '--cmd']
+
+                    subprocess.run(args)
+
+            json_data["quadrature_order"] = 4
             # #######################################################################
             print("------------------")
             print("spline")
